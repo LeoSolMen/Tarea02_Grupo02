7395b7166e7e4388624088790fc7388b6054d672
Leo Solis <lsolis10885@ufide.ac.cr>
2022-02-17 22:40:46 -0600

    Version final de la tarea 02 Grupo 02 Keren Jimenez Alejandro Martinez Leonel Solis

---
+++ src/main/java/paginaWeb/Tarea2DesarrolloApplication.java
@@ -0,0 +1,13 @@
+package paginaWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea2DesarrolloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea2DesarrolloApplication.class, args);
+	}
+
+}
